5422c4d0c44cac254ad8b2bd4f73cd8055698189
UID <UID@uid-PC>
2020-01-03 11:01:45 +0800

    提交

--- demo/src/test/MyTest.java
+++ demo/src/test/MyTest.java
@@ -2,7 +2,8 @@ package test;
 
 public class MyTest {
 	public static void main(String[] args) {
-		System.out.println("测试上传是否成功");
+		System.out.println("测试上传");
+	    System.out.println("嘻嘻");	
 	}
 
 }
